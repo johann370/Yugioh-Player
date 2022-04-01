1fa54a3a8d765cc84ebf473659994e46e2e1ec25
Johann Mendoza <johannmendoza370@gmail.com>
2022-03-31 22:53:11 -0500

    changed wording in trap class

--- Trap.py
+++ Trap.py
@@ -2,10 +2,10 @@ from Card import Card
 
 
 class Trap(Card):
-    def __init__(self, name, effect, spellType, spellSpeed):
+    def __init__(self, name, effect, trapType, spellSpeed):
         super().__init__(name, 'trap')
         self.effect = effect
-        self.spellType = spellType
+        self.trapType = trapType
         self.spellSpeed = spellSpeed
         self.turnSet = None
         self.options = ['Set']
